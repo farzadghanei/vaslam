2ebfcc58b377aaab8b0a818db599b1ead60e2d12
Farzad Ghanei <644113+farzadghanei@users.noreply.github.com>
2020-08-29 11:40:39 +0200

    Update packages metadata with package docs and license

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+"""
+tests
+=====
+
+vaslam test suite
+"""
+
+__license__ = "ISC License (ISCL)"
--- vaslam/__init__.py
+++ vaslam/__init__.py
@@ -1,3 +1,10 @@
+"""
+vaslam
+======
+
+Simple Internet connection diagnosis
+"""
+
 __author__ = "Farzad Ghanei <farzad.ghanei@tutanota.com>"
 __version__ = "0.0.1"
 __summary__ = "Simple Internet connection diagnosis"
