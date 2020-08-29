6db3ead08f7a8fc1a0dbcbb0c1c7cc2fe3fd2e36
Farzad Ghanei <644113+farzadghanei@users.noreply.github.com>
2020-08-29 11:26:14 +0200

    Improve the default configuration implementation and add tests

---
+++ tests/test_conf.py
@@ -0,0 +1,38 @@
+from vaslam.conf import (
+    Conf,
+    default_conf,
+    default_domains,
+    default_ipv4_name_servers,
+    default_ipv4_ping_hosts,
+    default_ipv4_echo_urls,
+)
+from unittest import TestCase
+from unittest.mock import patch
+
+
+class TestDefaultConf(TestCase):
+    def setUp(self):
+        patcher = patch("vaslam.conf.get_name_servers")
+        self.addCleanup(patcher.stop)
+        self.mock_get_ns = patcher.start()
+        self.mock_get_ns.return_value = ["192.168.0.100"]
+
+        patcher = patch("vaslam.conf.get_gateway_ipv4")
+        self.addCleanup(patcher.stop)
+        self.mock_get_gw = patcher.start()
+        self.mock_get_gw.return_value = "192.168.0.1"
+
+    def test_default_conf_returns_a_conf_with_defaults(self):
+        ret = default_conf()
+        self.assertIsInstance(ret, Conf)
+        self.assertEqual(ret.domains, default_domains)
+        self.assertEqual(ret.ipv4_default_name_servers, default_ipv4_name_servers)
+        self.assertEqual(ret.ipv4_ping_hosts, default_ipv4_ping_hosts)
+        self.assertEqual(ret.ipv4_echo_urls, default_ipv4_echo_urls)
+
+    def test_default_conf_returns_system_name_servers_and_default_gateway(self):
+        ret = default_conf()
+        self.assertEqual(ret.name_servers, ["192.168.0.100"])
+        self.mock_get_ns.assert_called_once_with()
+        self.assertEqual(ret.ipv4_gateway, "192.168.0.1")
+        self.mock_get_gw.assert_called_once_with()
--- vaslam/conf.py
+++ vaslam/conf.py
@@ -1,22 +1,23 @@
-from vaslam.system import get_name_servers, get_gateway_addr
+from vaslam.system import get_name_servers, get_gateway_ipv4
 
 
-default_name_servers = ("1.1.1.1", "8.8.8.8", "9.9.9.9")
-default_domains = ("fedoraproject.org", "opensuse.org", "icanhazip.com", "ifconfig.io")
-default_ping_hosts = ("1.1.1.1", "8.8.8.8", "9.9.9.9")
-default_ipv4_echo_urls = (
+default_domains = ["fedoraproject.org", "opensuse.org", "debian.org", "ubuntu.com"]
+default_ipv4_name_servers = ["1.1.1.1", "8.8.8.8", "9.9.9.9"]
+default_ipv4_ping_hosts = ["1.1.1.1", "8.8.8.8", "9.9.9.9"]
+default_ipv4_echo_urls = [
     "http://icanhazip.com/ip",
     "http://ifconfig.io/ip",
-    " http://ifconfig.me/ip",
-)
+    "http://ifconfig.me/ip",
+]
 
 
 class Conf:
     def __init__(self):
-        self.gateways = []  # type: List[str]
+        self.domains = []  # type: List[str]
         self.name_servers = []  # type: List[str]
-        self.default_name_servers = []  # type: List[str]
-        self.ping_hosts = []  # type: List[str]
+        self.ipv4_gateway = ""  # type: str
+        self.ipv4_default_name_servers = []  # type: List[str]
+        self.ipv4_ping_hosts = []  # type: List[str]
         self.ipv4_echo_urls = []  # type: List[str]
 
 
@@ -24,8 +25,8 @@ def default_conf() -> Conf:
     conf = Conf()
     conf.domains = default_domains
     conf.name_servers = get_name_servers()
-    conf.default_name_servers = default_name_servers
-    conf.ping_hosts = default_ping_hosts
-    conf.gateways = [get_gateway_addr()]
+    conf.ipv4_default_name_servers = default_ipv4_name_servers
+    conf.ipv4_ping_hosts = default_ipv4_ping_hosts
+    conf.ipv4_gateway = get_gateway_ipv4()
     conf.ipv4_echo_urls = default_ipv4_echo_urls
     return conf
